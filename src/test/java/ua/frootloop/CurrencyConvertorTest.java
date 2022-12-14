af697d62f25cdf4a56504dcad3b36e9d6baa35bf
AfraidOfCommits <alexandre.lecuyer.1@umontreal.ca>
2022-12-14 17:29:48 -0500

    - Updated to JUnit 5.9 - Finished specifications for Black Box testing. Will move on to White Box revision and planning before writing any code

--- src/test/java/ua/frootloop/CurrencyConvertorTest.java
+++ src/test/java/ua/frootloop/CurrencyConvertorTest.java
@@ -3,9 +3,10 @@ package ua.frootloop;
 public class CurrencyConvertorTest {
 
     /**
-     * ===========================
-     *      BLACK BOX TESTING
-     * ===========================
+     * ==================================================================================================
+     *                                          BLACK BOX TESTING
+     * ==================================================================================================
+     *
      * Selon nos spécifications, la méthode 'CurrencyConvertor.convert()' devrait;
      *    - Seulement accepter les devises : USD, CAD, GBP, EUR, CHF, INR, AUD
      *    - Seulement accepter les montants [0, 10000]
@@ -14,9 +15,9 @@ public class CurrencyConvertorTest {
      *      - Montants valides
      *      - Montants < 0
      *      - Montants > 10000
-     *      - Devises valides
-     *      - Devises incluses dans le dictionnaire "rates", mais non-incluses dans USD, CAD, GBP, EUR, CHF, INR, AUD
-     *      - Devises (Strings) qui ne sont pas des devises (non-incluses dans le dictionnaire "rates").
+     *      - Devises valides, i.e. elements de {USD, CAD, GBP, EUR, CHF, INR, AUD}
+     *      - Devises invalides qui sont dans le dictionnaire "rates" (voir le JSON).
+     *      - Devises invalides qui ne sont pas dans le dictionnaire "rates" (String random).
      *
      * Nous avons aussi les frontières (edge cases) à tester;
      *      - On utilisera une valeur à la borne des spécifications (à la borne d'équivalence), et une valeur typique.
@@ -25,10 +26,9 @@ public class CurrencyConvertorTest {
      *      - Pour les montants:
      *          - Montant = 0
      *          - Montant = 10000
-     *
-     *
-     *
      */
+    final static double AMOUNT_MINIMUM = 0.0d, AMOUNT_MAXIMUM = 10000.0d;
+    final static String[] CURRENCIES = new String[]{"USD", "CAD", "GBP", "EUR", "CHF", "INR", "AUD"};
 
 
 }
